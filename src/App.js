10d0e1004f1e6964d851949d01912419e5dcce16
Marina Sousa <marina.ijsousa@gmail.com>
2021-02-15 16:25:40 +0000

    remove container

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import Forecast from "./Forecast";
 export default function App() {
   return (
     <div className="App">
-    <div className="container">
         <Search />
         <Forecast />
-    </div>
     <footer id="pGitHub">
     <a href="https://github.com/MIJSousa/appreact" target="_blank" id="gitHub">Open-source code </a>
     by Marina Sousa
