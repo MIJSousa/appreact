eca21d869429a0035a11ea67de54725a2892b71a
Marina Sousa <marina.ijsousa@gmail.com>
2021-02-15 16:21:24 +0000

    export default function app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Search from "./Search";
 import Forecast from "./Forecast";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
     <div className="container">
@@ -18,5 +18,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
