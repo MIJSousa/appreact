52c8d0487ec2e543d8b155fc5ace2ae90e1d8149
Marina Sousa <marina.ijsousa@gmail.com>
2021-02-15 16:35:44 +0000

    add container

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Forecast from "./Forecast";
 export default function App() {
   return (
     <div className="App">
+    <div className="container">  
         <Search />
         <Forecast />
     <footer id="pGitHub">
     <a href="https://github.com/MIJSousa/appreact" target="_blank" id="gitHub">Open-source code </a>
     by Marina Sousa
     </footer>
+    </div>
     </div>
   );
 }
