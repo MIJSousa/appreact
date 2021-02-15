a273af03e72dd2ad16b06bbb291260388a27e699
Marina Sousa <marina.ijsousa@gmail.com>
2021-02-15 16:03:12 +0000

    import bootstrapp to index.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 import Search from "./Search";
 import Forecast from "./Forecast";
 
 function App() {
   return (
     <div className="App">
-      <div className="page">
+      <div className="container">
       <div>
         <Search />
         <Forecast />
       </div>
+      <p id="pGitHub">
+      <a href="https://github.com/MIJSousa/appreact" target="_blank" id="gitHub">Open-source code </a>
+      by Marina Sousa
+    </p>
     </div>
 
     </div>
