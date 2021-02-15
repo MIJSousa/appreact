c5a0a7f55d430d68b5d0e96a9fd08d9edddaa016
Marina Sousa <marina.ijsousa@gmail.com>
2021-02-15 16:19:16 +0000

    clean up folders

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
-
-
 import Search from "./Search";
 import Forecast from "./Forecast";
 
 function App() {
   return (
     <div className="App">
-    
-      <div className="container">
-        <div className="page">
-      <div>
+    <div className="container">
+    <div>
         <Search />
         <Forecast />
-      </div>
-     
+    </div>
     </div>
     <p id="pGitHub">
     <a href="https://github.com/MIJSousa/appreact" target="_blank" id="gitHub">Open-source code </a>
     by Marina Sousa
     </p>
-    </div>
     </div>
   );
 }
