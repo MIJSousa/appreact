fe90650141b105bb07a8fb80acc6cf32bc5dc92a
Marina Sousa <marina.ijsousa@gmail.com>
2021-02-15 16:24:54 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ export default function App() {
   return (
     <div className="App">
     <div className="container">
-    <div>
         <Search />
         <Forecast />
     </div>
-    </div>
-    <p id="pGitHub">
+    <footer id="pGitHub">
     <a href="https://github.com/MIJSousa/appreact" target="_blank" id="gitHub">Open-source code </a>
     by Marina Sousa
-    </p>
+    </footer>
     </div>
   );
 }
