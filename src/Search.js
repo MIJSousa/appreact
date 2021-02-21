1eb50b22c5efcded9e094a5d53428ff3b6469c07
Marina Sousa <marina.ijsousa@gmail.com>
2021-02-21 19:44:59 +0000

    added date

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,12 @@
 import React, {useState} from "react";
 import "./Search.css";
 import axios from "axios";
+import FormatDate from "./FormatDate";
 
 
 
 export default function Search() {
-  const [ready, setReady]=useState( false );
-  const [weather, setWeather]=useState({});
+  const [weather, setWeather]=useState({ready : false});
 
     function showWeather(response) {
     console.log(response);
@@ -18,12 +18,12 @@ export default function Search() {
     icon: response.data.weather[0].icon,
     date: new Date(response.data.dt * 1000),
     city: response.data.name,
+    ready: true
     });
-    setReady( true );
   }
 
 
-if(ready){
+if(weather.ready){
 return (
     <div className="search">
       <button id="current-location">📍 Current Location</button>
@@ -40,7 +40,9 @@ return (
       <div class="row today">
       <div class="col center extremes align-self-center">
         <h1 class="city" id="current-city"> {weather.city} </h1>
-        <h6 id="currentDate"></h6>
+        <h6 id="currentDate">
+          <FormatDate date={weather.date} />
+          </h6>
       </div>
       <div class="col center principal align-self-center">
         <ul>
@@ -80,7 +82,7 @@ return (
     </div>
   );
   }else{
-  let city="London";
+  let city="Porto";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3cd100e112e7defa0b76141c9b64f0fc&units=metric`;
   
   axios.get(url).then(showWeather);
